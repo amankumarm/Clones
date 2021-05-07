9ce67710f6e8436cd5e2afa0902f53db947027a4
amankumarm <amankumarm441@gmail.com>
2021-05-07 12:48:56 +0530

    work on charts

--- patient_dashboard/dashboard/src/Components/center.js
+++ patient_dashboard/dashboard/src/Components/center.js
@@ -1,19 +1,31 @@
 import React from 'react'
-
+import noti from '../assets/icon/notification(2).png'
+import ak from '../assets/icon/ak.jpg'
+import search_icon from '../assets/icon/loupe(2).png'
+import Graph from './micro/graph'
 export default function Centre() {
     return (
-        <div class="outer_center test">
-            <nav class="top_nav test">
-                <div class="nav_name ">
+        <div className="outer_center test">
+            <nav className="top_nav test">
+                <div className="nav_name ">
                     <h2>DashBoard</h2>
-                    <p class="welcome">Hi! Aman Kumar M</p>
+                    <p className="welcome">Hi! Aman Kumar M</p>
                 </div>
-                <div class="nav_inpt_div">
-                    <input type="text"  class="nav_input"/>
-
+                <div className="nav_inpt_div test">
+                    <div className="input_wrapper  ">
+                    <img src={search_icon} className=" test menu_icon search"  />
+                    <input type="text"  placeHolder="Search" className="nav_input test"/>
+                    </div>
+                </div>
+                <div className="noti test">
+                    <img src={noti}  className="test menu_icon "/>
+                    <img src={ak} id="profile_pic" className="test" />   
                 </div>
             </nav>
-            
+
+            <Graph />
         </div>
     )
 }
+
+
